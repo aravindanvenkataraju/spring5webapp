cb030eb847893996b6f6975d0ec5e6d8e895f9c7
Aravindan Venkataraju <aravindan.venkataraju@gmail.com>
2019-02-18 18:49:13 +1100

    add-repository-interfaces

---
+++ src/main/java/com/aravindan/springframework/model/repositories/BookRepository.java
@@ -0,0 +1,78 @@
+package com.aravindan.springframework.model.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.aravindan.springframework.model.Book;
+
+public interface BookRepository extends CrudRepository<Book, Long> {
+
+	@Override
+	default <S extends Book> S save(S entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	default <S extends Book> Iterable<S> saveAll(Iterable<S> entities) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	default Optional<Book> findById(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	default boolean existsById(Long id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	default Iterable<Book> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	default Iterable<Book> findAllById(Iterable<Long> ids) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	default long count() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	default void deleteById(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	default void delete(Book entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	default void deleteAll(Iterable<? extends Book> entities) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	default void deleteAll() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
