a865953bfcc2768dcdfde77d7e397596fdd7b120
Aravindan Venkataraju <aravindan.venkataraju@gmail.com>
2019-02-24 20:35:15 +1100

    add-book-controller

---
+++ src/main/java/com/aravindan/springframework/spring5webapp/controllers/BookController.java
@@ -0,0 +1,26 @@
+package com.aravindan.springframework.spring5webapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.aravindan.springframework.spring5webapp.model.repositories.BookRepository;
+
+@Controller
+public class BookController {
+	
+	private BookRepository bookRepository;
+	
+	
+	public BookController(BookRepository bookRepository) {
+		super();
+		this.bookRepository = bookRepository;
+	}
+
+	@RequestMapping("/books")
+	public String getBooks(Model model) {
+		
+		model.addAttribute("books", bookRepository.findAll());
+		return "books";
+	}
+}
